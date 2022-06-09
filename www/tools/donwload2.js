e4fd082ddd2234cc899cd4041300e45557ccd26b
matutosi <matutosi@gmail.com>
2022-06-09 17:16:06 +0900

    combine functions: download and getDate

--- www/tools/donwload2.js
+++ www/tools/donwload2.js
@@ -1,5 +1,4 @@
 // https://phper.pro/352
-// 一応動く
 
 function getNow(){
    var now = new Date();
@@ -9,36 +8,39 @@ function getNow(){
    const hh  = String(now.getHours()).padStart(2, `0`);
    const mi  = String(now.getMinutes()).padStart(2, `0`);
    const ss  = String(now.getSeconds()).padStart(2, `0`);
-   document.write(`${yr}_${mo}_${dd}_${hh}_${mi}_${ss}.tsv`)
+   return(`${yr}_${mo}_${dd}_${hh}_${mi}_${ss}.tsv`)
 }
 
 
-//CSV出力＆ダウンロード
-function handleDownload() {
-  var bom = new Uint8Array([0xEF, 0xBB, 0xBF]); //文字コードをBOM付きUTF-8に指定
-  var table = document.getElementById('table1'); //id=table1という要素を取得
-  var data_tsv=""; //ここに文字データとして値を格納していく
+window.addEventListener('load', () => {
+  const download_button = document.getElementById('download');
+  download_button.addEventListener('click', download_button_clicked);
+});
+
+function download_button_clicked(evt) {
+  evt.preventDefault();
+  var bom = new Uint8Array([0xEF, 0xBB, 0xBF]);  //set encoding UTF-8 with BOM
+  var table = document.getElementById('table1'); //get "id = table1"
+  var data_tsv = "";                             // data_tsv is data holder
 
   for(var i = 0;  i < table.rows.length; i++) {
     for(var j = 0; j < table.rows[i].cells.length; j++) {
-      data_tsv += table.rows[i].cells[j].innerText; //HTML中の表のセル値をdata_tsvに格納
-      if(j == table.rows[i].cells.length-1) data_tsv += "\n"; //行終わりに改行コードを追加
-      else data_tsv += "\t"; //セル値の区切り文字として\tを追加
+      data_tsv += table.rows[i].cells[j].innerText;           // save data in cellls
+      if(j == table.rows[i].cells.length-1) data_tsv += "\n";  // add line break
+      else data_tsv += "\t";                                   // add "\t" as separater
     }
   }
 
-  var blob = new Blob([ bom, data_tsv], { "type" : "text/tsv" }); //data_tsvのデータをtsvとしてダウンロードする関数
-  //     if (window.navigator.msSaveBlob) { //IEの場合の処理
-  //       window.navigator.msSaveBlob(blob, "test.tsv");
-  //     // window.navigator.msSaveOrOpenBlob(blob, "test.tsv");// msSaveOrOpenBlobの場合はファイルを保存せずに開ける
-  //   } else {
-  //     document.getElementById("download").href = window.URL.createObjectURL(blob);
+  var blob = new Blob([ bom, data_tsv], { "type" : "text/tsv" });  // download tsv data from data_tsv
+  const url = URL.createObjectURL(blob);
 
-  objUrl = window.URL.createObjectURL(blob);
-  const link = document.createElement("a");
-  link.href = objUrl;
-  link.download = getNow();
-  link.click();
+  const a = document.createElement("a");
+  document.body.appendChild(a);
+  a.download = getNow();
+  a.href = url;
+  a.click();
+  a.remove();
 
-  delete data_tsv; //data_tsvオブジェクトはもういらないので消去してメモリを開放
+  URL.revokeObjectURL(url);
+  delete data_tsv;
 }
