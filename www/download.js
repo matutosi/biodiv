527582280bd8f63e2f534a1c8d6fc24f141fb3e3
matutosi <matutosi@gmail.com>
2022-06-11 12:02:04 +0900

    move and rename

--- www/download.js
+++ www/download.js
@@ -1,26 +1,8 @@
 // https://phper.pro/352
 
-function getNow(){
-   var now = new Date();
-   const yr  = now.getFullYear();
-   const mo  = String(now.getMonth()).padStart(2, `0`);
-   const dd  = String(now.getDate()).padStart(2, `0`);
-   const hh  = String(now.getHours()).padStart(2, `0`);
-   const mi  = String(now.getMinutes()).padStart(2, `0`);
-   const ss  = String(now.getSeconds()).padStart(2, `0`);
-   return(`${yr}_${mo}_${dd}_${hh}_${mi}_${ss}.tsv`)
-}
-
-
-window.addEventListener('load', () => {
-  const download_button = document.getElementById('download');
-  download_button.addEventListener('click', download_button_clicked);
-});
-
-function download_button_clicked(evt) {
-  evt.preventDefault();
+function download(id){
   var bom = new Uint8Array([0xEF, 0xBB, 0xBF]);  //set encoding UTF-8 with BOM
-  var table = document.getElementById('target'); //get "id = target"
+  var table = document.getElementById(id);
   var data_tsv = "";                             // data_tsv is data holder
 
   for(var i = 0;  i < table.rows.length; i++) {
@@ -36,7 +18,7 @@ function download_button_clicked(evt) {
 
   const a = document.createElement("a");
   document.body.appendChild(a);
-  a.download = getNow();
+  a.download = getNow() + ".tsv";
   a.href = url;
   a.click();
   a.remove();
