f95090df0c1d37f8546bc83b4dea57d51dcfd0db
matutosi <matutosi@gmail.com>
2022-06-26 13:20:14 +0900

    change value for showing to users

--- www/js/fullScreen.js
+++ www/js/fullScreen.js
@@ -1,14 +1,17 @@
-// // // // // // // // // // // // // // // // // // // 
-// 
 // Switch full screen mode
-// 
-// // // // // // // // // // // // // // // // // // // 
 // https://gray-code.com/javascript/display-the-page-in-full-screen/
-function switchFullScreen() {
-  if( checkFullScreen() ) { document.exitFullscreen(); }
-  else                    { document.body.requestFullscreen(); }
+function switchScreenShow(id){
+  if( checkFullScreen() ) {
+    document.exitFullscreen(); 
+    var button = document.getElementById(id);
+    button.setAttribute("value", "TO FULL SCREEN");
+  } else {
+    document.body.requestFullscreen(); 
+    var button = document.getElementById(id);
+    button.setAttribute("value", "to normal show");
+  }
 }
-function checkFullScreen() {
+function checkFullScreen(){
   var fullscreen_flag = false;
   if(document.fullscreenElement) { fullscreen_flag = true; }
   return fullscreen_flag;
