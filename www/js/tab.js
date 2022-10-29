ea5b3370cda7d9d4609b71731739005fdd10fbd5
matutosi <matutosi@gmail.com>
2022-10-30 07:56:48 +0900

    add addTab in progress

--- www/js/tab.js
+++ www/js/tab.js
@@ -1,7 +1,3 @@
-// get elements
-var tabs = document.getElementById('tabcontrol').getElementsByTagName('a');
-var pages = document.getElementById('tabbody').getElementsByTagName('div');
-
 function changeTab(){
   var targetid = this.href.substring(this.href.indexOf('#')+1,this.href.length);
   // show delected tab
@@ -23,10 +19,28 @@ function changeTab(){
   return false;
 }
 
-// when clicked, enable to run changeTab() in all tab
-for(var i = 0; i < tabs.length; i++) {
-  tabs[i].onclick = changeTab;
+function updateTab(){
+  // get elements
+  var tabs = document.getElementById('tabcontrol').getElementsByTagName('a');
+  var pages = document.getElementById('tabbody').getElementsByTagName('div');
+  // when clicked, enable to run changeTab() in all tab
+  for(var i = 0; i < tabs.length; i++) {
+    tabs[i].onclick = changeTab;
+  }
 }
 
-// tab at first 
-tabs[0].onclick();
+// add a tab
+function addTab(obj){
+
+  id = prompt("表示させたい値", "初期値");
+  confirm();
+  var a = crEl({ el: 'a', ats: {href: "#" + id}, ih: id });
+  var div = crEl({ el: 'div', ats: {id: id} });
+  //   var a = crEl({ el: 'a' });
+  //   var div = crEl({ el: 'div' });
+  document.getElementById('tabcontrol').insertBefore(a, obj);
+  document.getElementById('tabbody').appendChild(div);
+
+  updateTab()
+  tabs[tabs.length - 1].onclick();  // move tab
+}
