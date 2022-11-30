cdefb653bf7b97eaf1d9cf9e727604dfaba9cc28
matutosi <matutosi@gmail.com>
2022-11-30 19:29:45 +0900

    move old wamei

--- www/tools/unused.js
+++ www/tools/unused.js
@@ -1,3 +1,34 @@
+// old codes
+function generateTable(data){
+  var table = crEl({ el: 'table' });
+  table.appendChild( crEl({ el: 'th', tc: "wamei" }) );
+  for(let i = 0; i < data.length; i++){
+    var tr = crEl({ el: 'tr' });
+    var td = crEl({ el: 'td', tc: data[i] });
+    tr.appendChild( td );
+    tr.style.display = 'none';
+    table.appendChild(tr);
+  }
+  return table
+}
+
+// wamei
+function generateSearchTable(ns, table){
+  // Up span
+  var up = crEl({ el:'span', ats:{id: "up_" + ns} });
+  up.appendChild( crEl({ el: 'B', tc: ns})  );
+  up.appendChild( createSearchShowInput()   );
+  up.appendChild( createSearchShowButton()  );
+  up.appendChild( createSearchWameiButton() );
+  // Table
+  var table = generateTable(wamei);
+  // Main
+  var main   = crEl({ el:'span', ats:{id: "main_"   + ns} });
+  main.appendChild(up);
+  main.appendChild(table);
+  return main;
+}
+
 // Fetch keys that begin with "bis_" from localStorage.
 //    @return    A string array of localStorage keys.
 function fetchLSKeys(){
